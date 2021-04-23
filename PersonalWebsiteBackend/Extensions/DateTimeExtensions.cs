2a176cf1f9eedf04be776ed31137b0808ca24934
unknown <thom.florian.1997@googlemail.com>
2021-04-23 20:57:15 +0200

    Added datetime extension

---
+++ PersonalWebsiteBackend/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalWebsiteBackend.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ConvertToISO8601String(this DateTime datetime)
+        {
+            return datetime.ToString("o");
+        }
+    }
+}
