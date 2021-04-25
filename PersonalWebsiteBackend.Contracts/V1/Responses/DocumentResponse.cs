f3e4ab7f105ffc4e3e8a8863bbb464385f889fa2
unknown <thom.florian.1997@googlemail.com>
2021-04-25 23:55:51 +0200

    Adjusted viewmodel

--- PersonalWebsiteBackend.Contracts/V1/Responses/DocumentResponse.cs
+++ PersonalWebsiteBackend.Contracts/V1/Responses/DocumentResponse.cs
@@ -8,15 +8,37 @@ namespace PersonalWebsiteBackend.Contracts.V1.Responses
     public class DocumentResponse
     { 
         public Guid Id { get; set; }
+        
+        public string DocumentId { get; set; }
 
         public string Name { get; set; }
 
         public string Description { get; set; }
 
-        public string Topic { get; set; }
+        public string OwnersEmail { get; set; }
 
-        public string Link { get; set; }
+        public string ThumbnailLink { get; set; }
 
+        public string WebcontentLink { get; set; }
+
+        public string WebviewLink { get; set; }
+
+        public string FileExtension { get; set; }
+
+        public string FullFileExtension { get; set; }
+
+        public string Kind { get; set; }
+
+        public string Md5Checksum { get; set; }
+
+        public bool Shared { get; set; }
+
+        public long Size { get; set; }
+
+        public long Version { get; set; }
+
+        public string DocumentCreatedTime { get; set; }
+        
         public string UserId { get; set; }
         
         public DateTime CreatedOn { get; set; }
